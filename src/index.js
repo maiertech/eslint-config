2219fdfb77186d2da32ef172ec5ef2c7d63adb6a
Thilo Maier <git@maier.asia>
2020-10-31 01:38:02 +0000

    Set "node/no-extraneous-require" to "error" Fixes #115

--- src/index.js
+++ src/index.js
@@ -9,6 +9,7 @@ const config = {
     'id-length': 'off',
     '@shopify/jsx-no-hardcoded-content': 'off',
     'babel/object-curly-spacing': ['error', 'always'],
+    'node/no-extraneous-require': 'error',
     'react/jsx-pascal-case': 'off',
   },
 };
