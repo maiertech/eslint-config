cfc69ee5db1dc06d2d286017fdc9e077ed37ece5
Thilo Maier <git@maier.asia>
2021-06-11 16:45:13 -0400

    Add import/order rule to sort imports Fixes #208

---
+++ .changeset/shaggy-coats-type.md
@@ -0,0 +1,5 @@
+---
+'@maiertech/eslint-config': major
+---
+
+Add import/order rule to impose sort order on imports
--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 module.exports = {
   extends: ['react-app', 'react-app/jest'],
+  rules: {
+    // Impose sort order on imports.
+    'import/order': [
+      'error',
+      {
+        groups: [
+          'builtin',
+          'external',
+          'internal',
+          'parent',
+          'sibling',
+          'index',
+          'object',
+          'type',
+        ],
+        'newlines-between': 'always',
+        alphabetize: {
+          order: 'asc',
+        },
+      },
+    ],
+  },
 };
